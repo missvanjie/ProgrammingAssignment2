3630fe81404b95e2e8b406cb77bde965028c8d1b
missvanjie <40222442+missvanjie@users.noreply.github.com>
2018-06-26 22:52:07 -0400

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## this function is where a special matrix is created. 
+## Object 'x' must be entered by a user.
+## Then it creates 4 functions known as the "getters and setter".
 
 makeCacheMatrix <- function(x = matrix()) {
+  s <- NULL
+  set <- function(i) {
+    x <<- i
+    s <<- NULL
+    
+    
+    
+  }
+  get <- function() x
+  setsolve <- function(solve) s <<- solve
+  getsolve <- function() s
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
+  
+  
 
 }
 
 
-## Write a short comment describing this function
+## This function either calculates the inverse if the chache inverse has been set to null
+## Or retrieves the cached inverse
+
+
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  s <- x$getsolve()
+  if(!is.null(s)){
+    message("cached data coming soon")
+    return(s)
+    
+  }
+  data <- x$get()
+  s <- solve(data, ...)
+  x$setsolve(s)
+  s
 }
